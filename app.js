538867fad6b074f4efeeb2890b19bf0affe31c9d
bilboandyoda <jordboudreau@gmail.com>
2018-01-15 23:39:20 -0800

    add search option

--- app.js
+++ app.js
@@ -12,6 +12,30 @@ app.get("/", function(req, res){
     res.render('home');
 });
 
+app.get("/search", function(req, res) {
+    res.render("search");
+});
+
+app.get("/searchResults", function(req, res) {
+    var coin = req.query.search.toLowerCase();
+    var errorMsg = "";
+    var data, price, trend;
+
+    request('https://api.coinmarketcap.com/v1/ticker/' + coin + '/?convert=USD', function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            data = JSON.parse(body);
+            price = data[0]["price_usd"].toLowerCase();
+            trend = data[0]["percent_change_7d"];
+        }
+        else {
+            errorMsg = "Sorry, that is not a valid coin.";
+            console.log(errorMsg);
+        }
+        res.render('index', { price: price, coin: coin, trend: trend });
+    });
+
+});
+
 app.get("/:coin", function(req, res) {
     var coin = req.params.coin.toLowerCase();
     var errorMsg = "";
